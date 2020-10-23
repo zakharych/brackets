9c3f92164c86eeae40d13476440ecf60404435f4
zakharych <alexzlk@yandex.ru>
2020-10-23 15:57:00 +0300

    feat : brackets finish

--- src/index.js
+++ src/index.js
@@ -1,45 +1,56 @@
-// module.exports = function check(str, bracketsConfig) {
-  function check(str, bracketsConfig) {
-    pairAarr = bracketsConfig[0];
+module.exports = function check(str, bracketsConfig) {
+        pairAarr = bracketsConfig[0];
 
-    var openBrackets = [];
-    var closeBrackets = [];
-    for (let i = 0; i < bracketsConfig.length; i++) {
-        let pair = bracketsConfig[i];
-        openBrackets.push(pair[0]);
-        closeBrackets.push(pair[1]);
-    }
+        var openBrackets = [];
+        var closeBrackets = [];
+        var ident = [];
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            var pair = bracketsConfig[i];
+            openBrackets.push(pair[0]); //создание массива открывающих скобок
+            closeBrackets.push(pair[1]); //создание массива закрывающих скобок
 
-    // console.log(str[0]);
-    rselt = true;
-    bufer = [];
-    for (let i = 0; i < str.length - 1; i++) {
-        if (openBrackets.includes(str[i])) {
-            bufer.push(str[i]);
-        } else {
-            index = "";
-            for (let j = 0; j < closeBrackets.length; j++) {
-                str[i] === closeBrackets[j] ? (index = j) : index;
-                console.log(`1-  + ${bufer}`);
-                bufer.pop(str[i-1]);
-                console.log(`2-  + ${bufer}`);
-
+            if (openBrackets[i] === closeBrackets[i]) {
+                ident.push(openBrackets[i]);
             }
-            if (openBrackets[index] !== str[i]) {
-                console.log(false);
-
-            }
-            // console.log(index);
         }
 
-        // console.log(str[i]);
-    }
-    console.log(bufer);
-}
+        rselt = true;
+        bufer = [];
 
-const config3 = [
-    ["(", ")"],
-    ["[", "]"],
-    ["{", "}"],
-];
-check("([{}])", config3);
+        for (let i = 0; i < str.length; i++) {
+            if (
+                !ident.includes(bufer[bufer.length - 1]) &&
+                ident.includes(str[i])
+            ) {
+                bufer.push(str[i]);
+            } else if (
+                openBrackets.includes(str[i]) &&
+                !ident.includes(str[i])
+            ) {
+                bufer.push(str[i]);
+            } else if (bufer.length === 0) {
+                return false;
+            } else {
+                index = "";
+                for (let j = 0; j < closeBrackets.length; j++) {
+                    if (
+                        str[i] === closeBrackets[j] &&
+                        bufer[bufer.length - 1] === openBrackets[j]
+                    ) {
+                        index = j;
+                        bufer.pop(str[i - 1]);
+                        break;
+                    } else if (!closeBrackets.includes(str[i])) {
+                        return false;
+                    }
+                }
+            }
+
+        }
+        if (bufer[0] === undefined) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+;
